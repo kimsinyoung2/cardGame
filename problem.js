c6c1b974e33ea5634a196190af248f834b5e7a76
sinyoung2 <105351819+kimsinyoung2@users.noreply.github.com>
2022-12-22 01:21:13 +0900

    Update problem.js

--- problem.js
+++ problem.js
@@ -1,12 +1,64 @@
 let problem = {
     prblib: [
         {
-            kor: "저는 김신영 입니다.",
-            eng: "my name is kim sin young",
+            kor: "과거에 연연하지 마세요.",
+            eng: "Don't dwell on the past",
         },
         {
-            kor: "두근거림을 떠올리는 거야.",
-            eng: "It reminds me of heart palpitations."
+            kor: "자기 자신을 믿으세요.",
+            eng: "Believe in yourself."
+        },
+        {
+            kor: "마음이 원하는 대로 따르세요.",
+            eng: "Follow your heart."
+        },
+        {
+            kor: "오늘을 즐기세요.",
+            eng: "Seize the day."
+        },
+        {
+            kor: "인생은 한 번 뿐이에요..",
+            eng: "You only live once."
+        },
+        {
+            kor: "과거는 과거입니다.",
+            eng: "Past is just past."
+        },
+        {
+            kor: "자기 자신을 사랑하세요.",
+            eng: "Love yourself."
+        },
+        {
+            kor: "자책하지 마세요.",
+            eng: "Don't beat yourself up."
+        },
+        {
+            kor: "인생은 여정이다.",
+            eng: "Life is a journey."
+        },
+        {
+            kor: "고통 없인 얻는 것도 없다.",
+            eng: "No pain no gain."
+        },
+        {
+            kor: "주사위는 던져졌다.",
+            eng: "The die is cast."
+        },
+        {
+            kor: "친구는 제2의 자신이다.",
+            eng: "A friend is a second myself."
+        },
+        {
+            kor: "외모는 속임수이다.",
+            eng: "Appearances are deceptive."
+        },
+        {
+            kor: "보는 것이 믿는 것이다.",
+            eng: "Seeing is believing."
+        },
+        {
+            kor: "지금 아니면 언제 하실 건가요?.",
+            eng: "If not now then when?."
         },
 
     ]
